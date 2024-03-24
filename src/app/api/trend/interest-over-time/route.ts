8f298bc435bfbb8b3bbb7be1917a234955c29935
jsglen1 <wtfdxd@gmail.com>
2024-03-24 11:07:18 -0500

    fix bad request 429 params much request api interest over time

--- src/app/api/trend/interest-over-time/route.ts
+++ src/app/api/trend/interest-over-time/route.ts
@@ -6,10 +6,9 @@ export async function POST(request: Request) {
   const { msj } = await request.json();
 
   return googleTrends
-    .interestOverTime({ keyword: msj, geo: "CO" })
+    .interestOverTime({ keyword: msj })
     .then(function (results: any) {
       const newData: ApiTimelineDataResponse = JSON.parse(results);
-      // console.log(JSON.stringify(newData));
       return Response.json({ results: newData });
     })
     .catch(function (err: any) {
